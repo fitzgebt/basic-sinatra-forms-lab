1c4ce23c4d9ea0a79736e1dcf8242f690f61c880
fitzgebt <fitzgebt@gmail.com>
2021-02-24 16:14:54 -0800

    commit tree 8ad9c22bbd7256baf14ad5f5d6b83163ab48d147

--- app.rb
+++ app.rb
@@ -2,5 +2,27 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get "/newteam" do 
+        erb :newteam
+    end
+
+    post "/newteam" do
+        name = params["name"]
+        coach = params["coach"]
+        pg = params["pg"]
+        sg = params["sg"]
+        pf = params["pf"]
+        sf = params["sf"]
+        c = params["c"]
+        @name = name
+        @coach = coach
+        @pg = pg
+        @sg = sg
+        @pf = pf
+        @sf = sf
+        @c = c 
+
+        erb :team
+    end
 
 end
